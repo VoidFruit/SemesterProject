c18735a718b5f9cb0635b5985554a55e9a6a2572
voidfruit <Julian.portillo@icloud.com>
2024-03-10 20:07:15 +0100

    fixed minor spelling mistake

--- public/js/game-manager.js
+++ public/js/game-manager.js
@@ -13,7 +13,7 @@ export class GameManager {
     this.iconSequence = [];
     this.playerSequence = [];
     this.iconDisplay = document.getElementById('icon');
-    this.icons = ['alarm-fill', 'heart-fill', 'airplane-fill', 'balloon-fill', 'battery-half', 'bicycle', 'book-fill', 'brilliance', 'bug-fill', 'camera-fill', 'capsule', 'clock-fill', 'arrow-through-heart-fil', 'cloud-drizzle-fill', 'compass-fill', 'dice-5-fill', 'dpad-fill', 'droplet-fill', 'egg-fill', 'eye-fill', 'feather', 'gear-fill', 'gift-fill', 'globe2', 'headphone', 'heart-fill', 'house-door-fill', 'icognito', 'lamp-fill', 'incognito', 'lightning-charge-fill', 'link', 'rocket-takeoff-fill', 'send-fill', 'shield-shaded', 'snow', 'speaker-fill', 'star-fill', 'suit-club-fill', 'suit-diamond-fill', 'suit-spade-fill', 'telephone-fill', 'tree-fill']; //icon pool
+    this.icons = ['alarm-fill', 'heart-fill', 'airplane-fill', 'balloon-fill', 'battery-half', 'bicycle', 'book-fill', 'brilliance', 'bug-fill', 'camera-fill', 'capsule', 'clock-fill', 'arrow-through-heart-fill', 'cloud-drizzle-fill', 'compass-fill', 'dice-5-fill', 'dpad-fill', 'droplet-fill', 'egg-fill', 'eye-fill', 'feather', 'gear-fill', 'gift-fill', 'globe2', 'headphone', 'heart-fill', 'house-door-fill', 'lamp-fill', 'incognito', 'lightning-charge-fill', 'link', 'rocket-takeoff-fill', 'send-fill', 'shield-shaded', 'snow', 'speaker-fill', 'star-fill', 'suit-club-fill', 'suit-diamond-fill', 'suit-spade-fill', 'telephone-fill', 'tree-fill']; //icon pool
   }
   //toggle the different container classes in index.html
   toggleGameState(newState) {
