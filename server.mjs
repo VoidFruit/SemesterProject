7598784e9a074055dd7db3c71c3a912c4f6d8d6c
voidfruit <Julian.portillo@icloud.com>
2024-03-16 23:22:43 +0100

    fixed typos

--- server.mjs
+++ server.mjs
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+
 import express from 'express' // Express is installed using npm
 import USER_API from './routes/usersRoute.mjs'; // This is where we have defined the API for working with users
 import SuperLogger from './modules/SuperLogger.mjs';
